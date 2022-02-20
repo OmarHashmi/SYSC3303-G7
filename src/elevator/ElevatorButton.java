552d1c78e7ff7875589cc0b6a33d6c6629c483ff
Thomas Pieroni <tjpieroni@gmail.com>
2022-02-19 21:14:34 -0500

    ElevatorClass Improvements & Documentation
    Added methods to elevator Lamp and Buttons class, implemented ArrivalSensors as list of 10 (max assumed floors) final documentation for iteration 2 and diagrams by Sam

--- src/elevator/ElevatorButton.java
+++ src/elevator/ElevatorButton.java
@@ -2,5 +2,16 @@ package elevator;
 
 //@author Tom P
 public class ElevatorButton {
+	private int floorNum;
+	private ElevatorLamp lamp;
 	
+	public ElevatorButton(int floorNum, ElevatorLamp lamp){
+		this.floorNum = floorNum;
+		this.lamp = lamp;
+	}
+	
+	// User presses button, light up corresponding lamp
+	public void press() {
+		this.lamp.turnOn();
+	}
 }
