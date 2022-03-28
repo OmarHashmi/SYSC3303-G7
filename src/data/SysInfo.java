4aa280854d15986e04755d5da0afcfaddcff161d
OmarHashmi <72258670+OmarHashmi@users.noreply.github.com>
2022-03-27 23:25:15 -0400

    tests-fix (#35)
    Fixed issue with tests not working with gui enabled

--- src/data/SysInfo.java
+++ src/data/SysInfo.java
@@ -1,21 +1,21 @@
 package data;
 
 public class SysInfo {
-	public static final boolean gui = true;
-	public static final boolean verbose = true;
+	public static boolean gui = true;
+	public static boolean verbose = true;
 	
-	public static final int numElevators = 4;
-	public static final int numFloors = 10;
+	public static int numElevators = 4;
+	public static int numFloors = 10;
 
-	public static final int elevatorSpeed = 1000;
-	public static final int errorTime = 10000;
-	public static final int floorSpeed = 500;
+	public static int elevatorSpeed = 1000;
+	public static int errorTime = 10000;
+	public static int floorSpeed = 500;
 	
-	public static final String   schedulerAddr = "127.0.0.1";
-	public static final String   floorAddr     = "127.0.0.1";
-	public static final String[] elevatorAddrs = {"127.0.0.1","127.0.0.1","127.0.0.1","127.0.0.1"};
+	public static String   schedulerAddr = "127.0.0.1";
+	public static String   floorAddr     = "127.0.0.1";
+	public static String[] elevatorAddrs = {"127.0.0.1","127.0.0.1","127.0.0.1","127.0.0.1"};
 	
-	public static final int   schedulerPort = 200;
-	public static final int   floorPort     = 201;
-	public static final int[] elevatorPorts = {202,203,204,205};
+	public static int   schedulerPort = 200;
+	public static int   floorPort     = 201;
+	public static int[] elevatorPorts = {202,203,204,205};
 }
